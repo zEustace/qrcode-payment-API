228e0c244e5bf412fb3f270c82638a2eca5fc94c
Thiag <cryptoeustace@gmail.com>
2024-08-27 17:52:24 -0300

    Adding the entire app

---
+++ app.py
@@ -0,0 +1,86 @@
+from db_models.payment import Payment
+from repository.database import db
+from payments.pix import Pix
+from datetime import datetime, timedelta
+from flask import Flask, jsonify, request, send_file, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
+
+db.init_app(app)
+socketio = SocketIO(app)
+
+
+@app.route('/payments/pix', methods=['POST'])
+def create_payment_pix():
+    data = request.get_json()
+
+    if 'value' not in data:
+        return jsonify({'message':'Invalid value'}), 400
+    expiration_date = datetime.now() + timedelta(minutes=30)
+
+    new_payment = Payment(value=data['value'], expiration_date=expiration_date)
+    pix_obj = Pix()
+    data_payment_pix = pix_obj.create_payment()
+    new_payment.bank_payment_id = data_payment_pix["bank_payment_id"]
+    new_payment.qr_code = data_payment_pix["qr_code_path"]
+    db.session.add(new_payment)
+    db.session.commit()
+    return jsonify({'message':'The payment has been created', "payment": new_payment.to_dict()})
+
+@app.route('/payments/pix/qr_code/<file_name>', methods=['GET'])
+def get_image(file_name):
+    return send_file(f"static/img/{file_name}.png", mimetype='image/png')
+
+@app.route('/payments/pix/confirmation', methods=['POST'])
+def pix_confirmation():
+    data = request.get_json()
+
+    if "bank_payment_id" not in data and "value" not in data:
+        return jsonify({'message':'Invalid payment data'}), 400
+
+    payment = Payment.query.filter_by(bank_payment_id=data.get('bank_payment_id')).first()
+
+    if not payment or payment.paid:
+        return jsonify({'message':'Payment not found'}), 404
+    
+    if data.get("value") != payment.value:
+        return jsonify({'message':'Invalid payment data'}), 400
+    
+    payment.paid = True
+    db.session.commit()
+    socketio.emit(f'payment-confirmed-{payment.id}')
+    return jsonify({'message':'The payment has been confirmed'})
+
+@app.route('/payments/pix/<int:payment_id>', methods=['GET'])
+def payment_pix_page(payment_id):
+    payment = Payment.query.get(payment_id)
+
+    if not payment:
+        return render_template('404.html')
+    
+    qr_code_filename = payment.qr_code.split('/')[-1]
+
+    if payment.paid:
+        return render_template('confirmed_payment.html', payment_id=payment.id, value=payment.value)
+
+    return render_template('payment.html',
+                           payment_id=payment.id, 
+                           value=payment.value, 
+                           host="http://127.0.0.1:5000", 
+                           qr_code=qr_code_filename)
+
+@socketio.on('connect')
+def handle_connect():
+    print("Client connected to the server")
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print("Client has disconnected from the server")
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
+
+
---
+++ db_models/payment.py
@@ -0,0 +1,20 @@
+from repository.database import db
+
+class Payment(db.Model):
+    # id, value, paid, bank payment ID, qrcode, expiration_date
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Float)
+    paid = db.Column (db.Boolean, default=False)
+    bank_payment_id = db.Column(db.String(300), nullable=True)
+    qr_code = db.Column(db.String(120), nullable=True)
+    expiration_date = db.Column(db.DateTime)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "value": self.value,
+            "paid": self.paid,
+            "bank_payment_id": self.bank_payment_id,
+            "qr_code": self.qr_code,
+            "expiration_date": self.expiration_date
+        }
